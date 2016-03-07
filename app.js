1b6fc64cba1147c71aef3d5745a2a94c1b934102
r.sinclairchin <r.sinclairchin@gmail.com>
2016-03-07 13:56:08 -0800

    add links to posts

--- app.js
+++ app.js
@@ -3,14 +3,23 @@ var app = angular.module('smackerNews', []);
 app.controller('MainCtrl', [
   '$scope',
   function($scope){
-    $scope.test = 'Hello World!'
     $scope.posts = [
     {title: 'You', upvotes: 5},
     {title: 'Me', upvotes: 2},
     {title: 'Nemo', upvotes: 9}
     ];
     $scope.addPost = function(){
-      $scope.posts.push({title: "A new post!", upvotes: 0})
-    }
+      if(!$scope.title || $scope.title === '') { return; }
+      $scope.posts.push({
+        title: $scope.title,
+        link: $scope.link,
+        upvotes: 0
+      });
+      $scope.title = '';
+      $scope.link = '';
+    };
+    $scope.incrementUpvotes = function(post){
+      post.upvotes += 1;
+    };
   }
   ])
